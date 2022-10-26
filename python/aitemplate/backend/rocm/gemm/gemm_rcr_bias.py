d40b431191254793769357467136d7aede73c01c
shaojiewang <wsjmessi@163.com>
2022-10-26 19:37:12 +0800

    add tensor stride fuse cat op on rocm

--- python/aitemplate/backend/rocm/gemm/gemm_rcr_bias.py
+++ python/aitemplate/backend/rocm/gemm/gemm_rcr_bias.py
@@ -22,7 +22,121 @@ from ... import registry
 from . import common
 from .layout import RCR
 
+import jinja2
+
 # pylint: disable=C0415,W0613
+
+PROBLEM_ARGS_TEMPLATE = jinja2.Template(
+    """
+{{indent}}                                static_cast<ck::half_t *>(in_ptr),
+{{indent}}                                static_cast<ck::half_t *>(weight_ptr),
+
+{% if gemm_flag == "bias_permute" %}
+{{indent}}                                static_cast<ck::half_t *>(bias_ptr),
+{% elif gemm_flag == "bias_permute_m2n3" %}
+{{indent}}                                std::array<const void*, 1>{static_cast<ck::half_t *>(bias_ptr)},
+{% elif gemm_flag == "permute_m2n3" %}
+{{indent}}                                {},
+{% else %}
+{% if "bias" in gemm_flag and not has_d0 %}
+{{indent}}                                std::array<const void*, 1>{static_cast<ck::half_t *>(bias_ptr)},
+{% elif has_d0 and not has_d1 %}
+{{indent}}                                std::array<const void*, 2>{static_cast<ck::half_t *>(bias_ptr),
+                                                                    static_cast<ck::half_t *>(d0_ptr)},
+{% elif has_d1 %}
+{{indent}}                                std::array<const void*, 3>{static_cast<ck::half_t *>(bias_ptr),
+                                                                    static_cast<ck::half_t *>(d0_ptr),
+                                                                    static_cast<ck::half_t *>(d1_ptr)},
+{% endif %}
+{% endif %}
+{{indent}}                                static_cast<ck::half_t *>(out_ptr) + output_offset,
+{% if gemm_flag not in ["permute_m2n3", "bias_permute_m2n3", "bias_permute_m3n2"]  %}
+{{indent}}                                M,
+{{indent}}                                N,
+{{indent}}                                K,
+{{indent}}                                stride_a,
+{{indent}}                                stride_b,
+{% endif %}
+{% if gemm_flag == "bias_permute" %}
+{{indent}}                                {M0, M1, M2, N0, N1, stride_D_M0, stride_D_M1, stride_D_M2, stride_D_N0, stride_D_N1},
+{{indent}}                                {M0, M1, M2, N0, N1, stride_E_M0, stride_E_M1, stride_E_M2, stride_E_N0, stride_E_N1},
+{% elif gemm_flag in ["permute_m2n3", "bias_permute_m2n3", "bias_permute_m3n2"]  %}
+{{indent}}                                a_ms_ks_lengths,
+{{indent}}                                a_ms_ks_strides,
+{{indent}}                                b_ns_ks_lengths,
+{{indent}}                                b_ns_ks_strides,
+    {% if gemm_flag == "permute_m2n3"  %}
+{{indent}}                                {},
+{{indent}}                                {},
+    {% else %}
+{{indent}}                                std::array<std::vector<ck::index_t>, 1>{d_ms_ns_lengths},
+{{indent}}                                std::array<std::vector<ck::index_t>, 1>{d_ms_ns_strides},
+    {% endif %}
+{{indent}}                                e_ms_ns_lengths,
+{{indent}}                                e_ms_ns_strides,
+{% else %}
+{% if "bias" in gemm_flag and not has_d0 %}
+{{indent}}                                std::array<ck::index_t, 1>{0},
+{% elif has_d0 and not has_d1 %}
+{{indent}}                                std::array<ck::index_t, 2>{0, static_cast<int>(stride_c)},
+{% elif has_d1 %}
+{{indent}}                                std::array<ck::index_t, 3>{0, static_cast<int>(stride_c), static_cast<int>(stride_c)},
+{% endif %}
+{{indent}}                                output_stride,
+{% endif %}
+{{indent}}                                ck::tensor_operation::element_wise::PassThrough{},
+{{indent}}                                ck::tensor_operation::element_wise::PassThrough{},
+{% if gemm_flag == "" %}
+{{indent}}                                ck::tensor_operation::element_wise::PassThrough{}
+{% elif gemm_flag == "permute_m2n3" %}
+{{indent}}                                ck::tensor_operation::element_wise::PassThrough{}
+{% elif gemm_flag == "bias" or "bias_permute" in gemm_flag %}
+{{indent}}                                ck::tensor_operation::element_wise::Add{}
+{% elif gemm_flag == "bias_relu" %}
+{{indent}}                                ck::tensor_operation::element_wise::AddRelu{}
+{% elif gemm_flag == "bias_fast_gelu" %}
+{{indent}}                                ck::tensor_operation::element_wise::AddFastGelu{}
+{% elif gemm_flag == "bias_swish" %}
+{{indent}}                                ck::tensor_operation::element_wise::AddHardswish{}
+{% elif gemm_flag == "bias_tanh" %}
+{{indent}}                                ck::tensor_operation::element_wise::AddTanh{}
+{% elif gemm_flag == "bias_sigmoid" %}
+{{indent}}                                ck::tensor_operation::element_wise::AddSigmoid{}
+{% elif gemm_flag == "bias_add" %}
+{{indent}}                                ck::tensor_operation::element_wise::AddAdd{}
+{% elif gemm_flag == "bias_mul" %}
+{{indent}}                                ck::tensor_operation::element_wise::AddMul{}
+{% elif gemm_flag == "bias_mul_tanh" %}
+{{indent}}                                ck::tensor_operation::element_wise::AddMulTanh{}
+{% elif gemm_flag == "bias_add_relu" %}
+{{indent}}                                ck::tensor_operation::element_wise::AddAddRelu{}
+{% elif gemm_flag == "bias_add_fast_gelu" %}
+{{indent}}                                ck::tensor_operation::element_wise::AddAddFastGelu{}
+{% elif gemm_flag == "bias_sigmoid_mul" %}
+{{indent}}                                ck::tensor_operation::element_wise::AddSigmoidMul{}
+{% elif gemm_flag == "bias_sigmoid_mul_tanh" %}
+{{indent}}                                ck::tensor_operation::element_wise::AddSigmoidMulTanh{}
+{% elif gemm_flag == "bias_mul_add" %}
+{{indent}}                                ck::tensor_operation::element_wise::AddMulAdd{}
+{% elif gemm_flag == "bias_add_add" %}
+{{indent}}                                ck::tensor_operation::element_wise::AddAddAdd{}
+{% elif gemm_flag == "bias_add_add_relu" %}
+{{indent}}                                ck::tensor_operation::element_wise::AddAddAddRelu{}
+{% endif %}
+"""
+)
+
+OUTPUT_ADDR_CALCULATOR = jinja2.Template(
+    """
+  {% if not output_accessor.is_from_strided_tensor %}
+  int64_t output_stride = {{stride_dim}};
+  int64_t output_offset = 0;
+  {% else %}
+  int64_t output_stride = {{output_accessor.actual_total_elements_from_stride_dim}};
+  int64_t output_offset = {{output_accessor.offset}};
+  {% endif %}
+    """
+)
 
 
 @registry.reg("rocm.gemm_rcr_bias.config")
@@ -90,7 +204,20 @@ def gemm_gen_function(func_attrs, exec_cond_template, dim_info_dict):
     str
         The rendered template of generated function body.
     """
-    return common.gen_function(func_attrs, exec_cond_template, dim_info_dict, "bias")
+    #input_addr_calculator = gemm_rcr.get_input_addr_calculator(func_attrs)
+    input_ndims = len(func_attrs["input_accessors"][0].original_shapes)
+    weight_ndims = len(func_attrs["input_accessors"][1].original_shapes)
+    output_ndims = len(func_attrs["output_accessors"][0].original_shapes)
+    print(input_ndims)
+    return common.gen_function(
+        func_attrs, 
+        exec_cond_template, 
+        dim_info_dict, 
+        "bias",
+        problem_args_template = PROBLEM_ARGS_TEMPLATE,
+        output_addr_calculator=OUTPUT_ADDR_CALCULATOR.render(
+            stride_dim="N", output_accessor=func_attrs["output_accessors"][0]
+        ))
 
 
 @registry.reg("rocm.gemm_rcr_bias.func_decl")
