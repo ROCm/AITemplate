f82317167e6b03505fa08dff4adfd4e8893fd133
aska-0096 <haocwang@amd.com>
2023-06-13 10:45:21 +0000

    deprecate unused header file

--- static/include/rocm_includes.h
+++ static/include/rocm_includes.h
@@ -1,5 +1,4 @@
 #include "include/ck/tensor_operation/gpu/device/tensor_layout.hpp"
-#include "include/ck/utility/print.hpp"
 #include "library/include/ck/library/utility/device_memory.hpp"
 #include "library/include/ck/library/utility/host_tensor.hpp"
 #include "library/include/ck/library/utility/host_tensor_generator.hpp"
